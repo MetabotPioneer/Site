5deac95223be39c9d8f260d2d0d3f47260d29892
MetabotPioneer <135253300+MetabotPioneer@users.noreply.github.com>
2023-09-01 14:34:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,33 @@ sessionStorage.setItem('CUSTOM_EVENT_DATASET', JSON.stringify({
             "service": service,
           }));
 
+<script>
+    function changeUrl(title, url) {
+        if (typeof (history.pushState) != "undefined") {
+            var obj = { Title: title, Url: url };
+            history.pushState(obj, obj.Title, obj.Url);
+        } else {
+            alert("Browser does not support HTML5.");
+        }
+    }
+
+    $(function () {
+      $('body').on('click', '.open-sevice', function (ev) {
+        ev.preventDefault();
+        let title = $(this).data('title');
+        let service = $(this).data('service');
+        changeUrl(title, $(this).data('href'));
+        $('#current-page').html(title);
+
+        if (service.length > 0) {
+          sessionStorage.setItem('CUSTOM_EVENT_DATASET', JSON.stringify({
+            "service": service,
+          }));
+        } else {
+          sessionStorage.setItem('CUSTOM_EVENT_DATASET', JSON.stringify({}));
+        }
+      });
+
+              sessionStorage.setItem('CUSTOM_EVENT_DATASET', JSON.stringify({}));
+          });
+  </script>
